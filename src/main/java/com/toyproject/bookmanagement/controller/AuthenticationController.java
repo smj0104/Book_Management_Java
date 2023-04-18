6202ce2c538a369c15c8b89777d1ecac12a05d52
DESKTOP-NUOPUIF\ITPS <hikoseki1997@naver.com>
2023-04-18 17:43:01 +0900

    0418 Spring book_management

--- src/main/java/com/toyproject/bookmanagement/controller/AuthenticationController.java
+++ src/main/java/com/toyproject/bookmanagement/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,8 @@ public class AuthenticationController {
 	public ResponseEntity<?> login() {
 		return ResponseEntity.ok(null);
 	}
+	
+	@CrossOrigin
 	@ValidAspect
 	@PostMapping("/signup")
 	public ResponseEntity<?> signup(@Valid @RequestBody SignupReqDto signreqDto, BindingResult bindingResult) {
--- src/main/java/com/toyproject/bookmanagement/dto/auth/SignupReqDto.java
+++ src/main/java/com/toyproject/bookmanagement/dto/auth/SignupReqDto.java
@@ -11,7 +11,7 @@ public class SignupReqDto {
 	@Email
 	private String email;
 	
-	@Pattern(regexp = " ^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$",
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$",
 			message = "비밀번호는 영문자, 숫자, 특수문자를 포함하여 8 ~ 16자로 작성하세요.")	//^정규식 시작 $종료 ?=앞쪽이 일치하는지 확인  .*모든글자가 여러개 있거나없거나
 	private String password;	// *\\ 숫자를 하나 포함해야한다(이 중 하나라도 포함하지않을경우 false)  [A-Za-z\\d@$!%*#?&]포함할수있는 글자  {8,} = 글자갯수(최소값,최대값)
 	
